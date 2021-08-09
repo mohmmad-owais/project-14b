48b7519ebbf06b14c3aadb2644ff570c83c2a44d
Owais <precisework01@gmail.com>
2021-08-10 01:34:42 +0500

    update2

--- bookmark-frontend-13b/src/amplifyContext/client.tsx
+++ bookmark-frontend-13b/src/amplifyContext/client.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react"
 import Amplify from "aws-amplify"
-import awsmobile from "./aws-exports"
+import awsmobile from "../aws-exports"
 
 interface props {
   children: ReactNode
